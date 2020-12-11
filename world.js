a743db519c2bf34a79b08be3076a0a874787b2fc
Deandrew007 <deamoore07@gmail.com>
2020-12-11 08:21:51 -0500

    added js

---
+++ world.js
@@ -0,0 +1,25 @@
+window.onload = world;
+function world(){
+    let searchbtn = document.getElementById('lookup');
+    let searchbtn2 = document.getElementById('lookup2');
+    let msg = document.getElementById('result');
+    searchbtn.addEventListener('click', function(element) {
+        element.preventDefault();
+        var myform = document.getElementById("country").value; 
+        fetch("world.php"+"? country="+myform)
+        .then(response => {
+            if (response.ok) {
+                return response.text()
+            } else {
+                return Promise.reject('sumn nuh right')
+            }
+        })
+        .then(data => {
+            msg.innerHTML = data;
+        })
+        .catch(error => console.log('error: ' + error));
+
+    });
+
+
+}
