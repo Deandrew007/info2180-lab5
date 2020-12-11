7e396ac043f0cbef5c43c54d13eec23d63c04e7c
Deandrew007 <deamoore07@gmail.com>
2020-12-11 08:48:11 -0500

    added btn2 js and minor changes

--- world.js
+++ world.js
@@ -1,7 +1,7 @@
 window.onload = world;
 function world(){
     let searchbtn = document.getElementById('lookup');
-    let searchbtn2 = document.getElementById('lookup2');
+    let searchbtn2 = document.getElementById('citylookup');
     let msg = document.getElementById('result');
     searchbtn.addEventListener('click', function(element) {
         element.preventDefault();
@@ -20,6 +20,23 @@ function world(){
         .catch(error => console.log('error: ' + error));
 
     });
-
+    searchbtn2.addEventListener('click', function(element) {
+        element.preventDefault();
+        var myform = document.getElementById("country").value; 
+        
+        fetch("world.php"+"?country="+myform +"&context=cities")
+        .then(response => {
+            if (response.ok) {
+                return response.text()
+            } else {
+                return Promise.reject('sumn nuh right')
+            }
+        })
+        .then(data => {
+            msg.innerHTML = data;
+        })
+        .catch(error => console.log('error: ' + error));
+           
+    });
 
 }
